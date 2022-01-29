bc59385ad1317735abe2013159233090e4b919c4
himal999 <himal.official99@gmail.com>
2022-01-29 09:00:22 +0530

    Move All page index.html file

---
+++ frontend/script.js
@@ -0,0 +1,15 @@
+
+
+
+document.getElementById("cuView").style.display = "none";
+document.getElementById("cuAdd").style.display = "none";
+document.getElementById("cuDelete").style.display = "none";
+document.getElementById("cuUpdate").style.display = "none";
+
+
+document.getElementById("itView").style.display = "none";
+document.getElementById("itAdd").style.display = "none";
+document.getElementById("itDelete").style.display = "none";
+document.getElementById("itUpdate").style.display = "none";
+
+document.getElementById("order").style.display = "none";
